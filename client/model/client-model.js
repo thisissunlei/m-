449ea799f3e001bcc892ffb32700f24909ac3ad8
lifan <lifan@gongsibao.com>
2019-01-22 15:10:01 +0800

    fix: merge

--- client/model/client-model.js
+++ client/model/client-model.js
@@ -290,19 +290,19 @@ export default {
   //获取定位社区
   getVisitCommunity(data) {
     return handleRequest(
-      request.get('/api/gateway/get-community-byip')
+      request.get('api/gateway/get-community-byip')
     );
   },
   // 获取全部城市
   getCityList(data) {
     return handleRequest(
-      request.get('/api/gateway/protals-all-city')
+      request.get('api/gateway/protals-all-city')
     );
   },
   //market推广预约
   postCityVisit(data) {
     return handleRequest(
-      request.post('/api/gateway/portals/save-city-visit',
+      request.post('api/gateway/portals/save-city-visit',
         Qs.stringify(data)
       )
     );
